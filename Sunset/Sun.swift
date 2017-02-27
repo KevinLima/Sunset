26a87c8d08a23408e3f4a6f9de80e4a6045c9ebe
Kevin Lima <cknl360@gmail.com>
2017-02-27 18:21:46 +0100

    started with retrieving sun profile
    —

--- Sunset/Sun.swift
+++ Sunset/Sun.swift
@@ -12,8 +12,14 @@ class Sun{
 	private var sunset: String = "18:19"
 	private var sunrise: String = "07:31"
 	
-
 	private var location: Location = Location()
+	
+	private var isDownloaded: Bool = false
+	private var results = [String]()
+	
+	init() {
+		getJSON()
+	}
 	
 	
 	func getSunset()->String{
@@ -26,6 +32,23 @@ class Sun{
 	
 	func getLocation()->String{
 		return location.getCity()
+	}
+	
+	func getJSON(){
+		let requestURL: NSURL = NSURL(string: "http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400")!
+		let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+		let session = URLSession.shared
+		let task = session.dataTask(with: urlRequest as URLRequest){
+			(data, response, error) -> Void in
+			
+			let httpResponse = response as! HTTPURLResponse
+			let statusCode = httpResponse.statusCode
+			
+			if statusCode == 200{
+				print(data)
+			}
+		}
+		task.resume()
 	}
 	
 }
