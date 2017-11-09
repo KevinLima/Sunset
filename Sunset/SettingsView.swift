c4df7a54359f7945e33ae8644af9088334ac21e5
Kevin Lima <cknl360@gmail.com>
2017-11-09 15:13:15 +0100

    created settings view

---
+++ Sunset/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  Sunset
+//
+//  Created by Kevin Lima on 09/11/2017.
+//  Copyright © 2017 Kevin Lima. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
